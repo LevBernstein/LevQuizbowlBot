b3120b599e23f3776bb19609dcf601d27e5f49bb
LevBernstein <lev2000@me.com>
2020-11-14 23:54:57 -0500

    Changed the order of commands in tutorial in order to reduce space

--- QBBot.py
+++ QBBot.py
@@ -1,6 +1,6 @@
 #Lev's Quizbowl Bot
 #Author: Lev Bernstein
-#Version 1.3.0
+#Version 1.3.1
 
 
 import discord
@@ -126,27 +126,27 @@ async def on_message(text):
         if text.content.startswith('!team '): #Teams require the following roles: Team red, Team blue, Team green, Team orange, Team yellow, Team purple
             print("calling team")
             report = "Invalid role!"
-            if text.content.startswith('!team red'):
+            if text.content.startswith('!team r'):
                 role = get(text.guild.roles, name = 'Team red')
                 await text.author.add_roles(role)
                 report = "Gave you the role, " + text.author.mention + "."
-            if text.content.startswith('!team blue'):
+            if text.content.startswith('!team b'):
                 role = get(text.guild.roles, name = 'Team blue')
                 await text.author.add_roles(role)
                 report = "Gave you the role, " + text.author.mention + "."
-            if text.content.startswith('!team green'):
+            if text.content.startswith('!team g'):
                 role = get(text.guild.roles, name = 'Team green')
                 await text.author.add_roles(role)
                 report = "Gave you the role, " + text.author.mention + "."
-            if text.content.startswith('!team orange'):
+            if text.content.startswith('!team o'):
                 role = get(text.guild.roles, name = 'Team orange')
                 await text.author.add_roles(role)
                 report = "Gave you the role, " + text.author.mention + "."
-            if text.content.startswith('!team yellow'):
+            if text.content.startswith('!team y'):
                 role = get(text.guild.roles, name = 'Team yellow')
                 await text.author.add_roles(role)
                 report = "Gave you the role, " + text.author.mention + "."
-            if text.content.startswith('!team purple'):
+            if text.content.startswith('!team p'):
                 role = get(text.guild.roles, name = 'Team purple')
                 await text.author.add_roles(role)
                 report = "Gave you the role, " + text.author.mention + "."
@@ -227,7 +227,7 @@ async def on_message(text):
                 report = "You need to start a game first! Use '!start' to start a game."
             await text.channel.send(report)
         
-        if isInt(text.content): #Assigns points. Checks if bot because, otherwise, the Bot's own embedded messages would throw errors.
+        if len(games) != 0 and isInt(text.content): #Assigns points. Checks if bot because, otherwise, the Bot's own embedded messages would throw errors. Checks len games to avoid unnecessary calls.
             print("calling points")
             print(text.content + " is an int")
             current = text.channel.id
@@ -372,20 +372,21 @@ async def on_message(text):
         if text.content.startswith('!help') or text.content.startswith('!commands') or text.content.startswith('!tutorial'):
             print("calling tutorial")
             emb = discord.Embed(title="Lev's Quizbowl Bot Commands", description="", color=0x57068C)
-            emb.add_field(name= "!start", value= "Starts a new game.", inline=False)
-            emb.add_field(name= "buzz", value= "Buzzes in.", inline=False)
-            emb.add_field(name= "Any positive or negative whole number", value= "After a buzz or a bonus, the reader can enter a whole number to assign points.", inline=False)
-            emb.add_field(name= "!clear", value= "Clears buzzes without advancing the TU count.", inline=False)
-            emb.add_field(name= "!dead", value= "Clears buzzes after a dead TU and advances the TU count.", inline=False)
-            emb.add_field(name= "!score", value= "Displays the score, sorted from highest to lowest.", inline=False)
-            emb.add_field(name= "!end", value= "Ends the active game.", inline=False)
-            emb.add_field(name= "!team [red/blue/green/orange/yellow/purple]", value= "Assigns you the team role corresponding to the color you entered.", inline=False)
-            emb.add_field(name= "!call", value= "Mentions everyone in the server and informs them that it is time for practice. Usable only by admins.", inline=False)
-            emb.add_field(name= "!github", value= "Gives you a link to this bot's github page.", inline=False)
-            emb.add_field(name= "!report", value= "Gives you a link to this bot's issue-reporting page.", inline=False)
-            emb.add_field(name= "!tu", value= "Reports the current tossup number.", inline=False)
-            emb.add_field(name= "!bonusmode", value= "Toggles if bonuses are enabled. They are enabled by default.", inline=False)
-            emb.add_field(name= "!bstop", value= "Kills an active bonus without giving points.", inline=False)
+            emb.add_field(name= "!start", value= "Starts a new game.", inline=True)
+            emb.add_field(name= "buzz", value= "Buzzes in.", inline=True)
+            emb.add_field(name= "!end", value= "Ends the active game.", inline=True)
+            emb.add_field(name= "!clear", value= "Clears buzzes without advancing the TU count.", inline=True)
+            emb.add_field(name= "!dead", value= "Clears buzzes after a dead TU and advances the TU count.", inline=True)
+            emb.add_field(name= "!score", value= "Displays the score, sorted from highest to lowest.", inline=True)
+            emb.add_field(name= "Any whole number", value= "After a buzz or a bonus, the reader can enter a +/- whole number to assign points.", inline=True)
+            emb.add_field(name= "!team [r/b/g/o/y/p]", value= "Assigns you the team role corresponding to the color you entered.", inline=True)
+            emb.add_field(name= "!call", value= "Mentions everyone in the server and informs them that it is time for practice. Usable only by admins.", inline=True)
+            emb.add_field(name= "!github", value= "Gives you a link to this bot's github page.", inline=True)
+            emb.add_field(name= "!report", value= "Gives you a link to this bot's issue-reporting page.", inline=True)
+            emb.add_field(name= "!tu", value= "Reports the current tossup number.", inline=True)
+            emb.add_field(name= "!bonusmode", value= "Disables or enables. Bonuses are enabled by default.", inline=True)
+            emb.add_field(name= "!bstop", value= "Kills an active bonus without giving points.", inline=True)
+            emb.add_field(name= "!tutorial", value= "Shows you this list.", inline=True)
             await text.channel.send(embed=emb)
             
             #await text.channel.send('Valid commands: \r\n "!start" starts a new game. \r\n "buzz" buzzes in. \r\n Enter any positive or negative whole number after someone buzzes to assign points. \r\n "!clear" clears buzzers after a TU goes dead. \r\n "!score" displays the score, sorted from highest to lowest. \r\n "!end" ends the active game. \r\n "!team [red/blue/green/orange/yellow/purple]" assigns you the team role corresponding to the color you entered. \r\n "!call" or "!summon" mentions everyone in the server and informs them it is time for practice \r\n "!github" gives you a link to this bot\'s github page. \r\n "!report" gives you a link to this bot\'s issue-reporting page. ')
