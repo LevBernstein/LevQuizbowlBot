414b4521dd1c5126c2da567f59cf05a57ba98b23
LevBernstein <lev2000@me.com>
2021-01-25 22:20:18 -0500

    Safety for avatar updating

--- QBBot.py
+++ QBBot.py
@@ -1,6 +1,6 @@
 # Lev's Quizbowl Bot
 # Author: Lev Bernstein
-# Version: 1.7.11
+# Version: 1.7.12
 # This bot is designed to be a user-friendly Quizbowl Discord bot with a minimum of setup.
 # All commands are documented; if you need any help understanding them, try the command !tutorial.
 # This bot is free software, licensed under the GNU GPL version 3. If you want to modify the bot in any way,
@@ -501,10 +501,10 @@ class Instance: # instance of an active game. Each channel a game is run in gets
             if searching and changed == 2:
                 lastLine[3] = str(points)
                 searching = False
-            if searching and changed == 3
+            if searching and changed == 3:
                 lastLine[4] = str(points)
                 searching = False
-            if searching and changed == 4
+            if searching and changed == 4:
                 lastLine[5] = str(points)
                 searching = False
             if searching and changed == 5:
@@ -616,10 +616,13 @@ async def on_message(text):
                     await text.guild.create_custom_emoji(name = 'power', image = img)
                 print("Emojis live!")
                 
-                g = open("templates/pfp.png", "rb")
-                pic = g.read()
-                await client.user.edit(avatar=pic) # Running !setup too many times will cause the Discord API to deny API calls that try to change the profile picture.
-                print("Avatar live!")
+                with open("templates/pfp.png", "rb") as pfp:
+                    pic = pfp.read()
+                    try:
+                        await client.user.edit(avatar=pic) # Running !setup too many times will cause the Discord API to deny API calls that try to change the profile picture.
+                        print("Avatar live!")
+                    except discord.HTTPException: # In case of the above issue:
+                        print("Avatar failed to update!")
     
                 report = "Successfully set up the bot! Team roles now exist, as do the following emojis: buzz, power, ten, neg."
             await text.channel.send(report)
